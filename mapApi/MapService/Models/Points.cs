835aa7040407ce1768e3ffdac39e1f55c71354ed
surevil <445073309@qq.com>
2018-03-05 12:36:15 +0800

    .NET webapi complete

---
+++ mapApi/MapService/Models/Points.cs
@@ -0,0 +1,10 @@
+﻿namespace MapService.Models
+{
+    public class Points
+    {
+        public double fromLng { get; set; }
+        public double fromLat { get; set; }
+        public double toLng { get; set; }
+        public double toLat { get; set; }
+    }
+}
